ad746bdb47741d763f8c0764a4bd91cc7432c611
Glitch (hello-webpage) <none>
2020-05-07 17:52:26 +0000

    🐥🏞 Checkpoint ./script.js:15945968/443

--- script.js
+++ script.js
@@ -2,18 +2,23 @@
 
 function setup() {
   // Code here runs only once
-  createCanvas(800, 100);
+  createCanvas(800, 500);
 }
 
 function draw() {
   // Code here runs continuously
   // background(200,100,133);
-  var size = 50;
+  var size = 100;
   noFill();
-  strokeWeight(1);
-  ellipse(100, 50, size, size);
-  ellipse(155, 50, size, size);
-  ellipse(1, 50, size, size);
-  ellipse(125, 50, size, 50);
-  ellipse(150, 50, 50, 50);
+  strokeWeight(8);
+  stroke(100, 150, 255);
+  ellipse(100, 100, size);
+  stroke(255, 200, 0);
+  ellipse(155, 150, size);
+  stroke('black');
+  ellipse(210, 100, size);
+  stroke(50,155,50);
+  ellipse(265, 150, size);
+  stroke('red');
+  ellipse(320, 100, size);
 }
