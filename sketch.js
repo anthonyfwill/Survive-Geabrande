01e32c864c795ef1d8b513b2499ad2984107e544
Glitch (hello-webpage) <none>
2020-07-05 07:23:10 +0000

    💵🚇 Checkpoint ./README.md:15945968/1330 ./sketch.js:15945968/1099 ./index.html:15945968/308

---
+++ sketch.js
@@ -0,0 +1,13 @@
+/* global createCanvas background ellipse */
+
+function setup(){
+  // Code here runs only once
+  createCanvas(800, 600)
+}
+
+function draw(){
+  // Code here runs continuously
+  background(220)
+
+  ellipse(50, 50, 50, 50)
+}
