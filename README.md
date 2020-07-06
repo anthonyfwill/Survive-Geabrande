0759f138da845c6765ca3bdf10eacd020a3bd217
Glitch (hello-webpage) <none>
2020-07-06 09:15:33 +0000

    ⛸❣️ Checkpoint ./.glitch-assets:15945968/86 ./README.md:15945968/27 ./index.html:15945968/177 ./style.css:15945968/71 ./sketch.js:15945968/543

--- README.md
+++ README.md
@@ -5,5 +5,4 @@ CSSI Day 1
 
 - README.md - This file!
 - index.html - Html page base structure, we include dependencies like JS files here
-- sketch.js - Your p5js code goes here.
-- assets/ - example logos
+- sketch.js - Your p5js code goes here
--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
-/* global createCanvas background ellipse */
+/* global createCanvas background image loadImage */
 
-function setup() {
-  // Code here runs only once
+let dvdImage;
+
+function setup(){
   createCanvas(800, 600);
+  // We only want to load the logo once.
+  dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
 }
 
-function draw() {
-  // Code here runs continuously
+function draw(){
   background(220);
-
-  // ellipse(50, 50, 50, 50)
+  // Draw the logo at the new position.
+  image(dvdImage, 50, 50, 200, 150);
 }
