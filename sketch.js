a9d394761c714526d8d83c8b55a8aedd0762e332
Glitch (hello-webpage) <none>
2020-07-07 04:30:56 +0000

    🏦🐓 Checkpoint ./sketch.js:15945968/566

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 /* global createCanvas background image loadImage */
 
-let dvdImage;
+let dvdImage, imageX;
 
 function setup(){
-  createCanvas(800, 600);
+  createCanvas(400, 400);
+  imageX = 0;
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/2c8c05ca-f0b5-426e-9d1a-07a2d87bd9f6%2Fdvd-video.jpeg?v=1594094901767");
 }
@@ -11,5 +12,7 @@ function setup(){
 function draw(){
   background(220);
   // Draw the logo at the new position.
-  image(dvdImage, 50, 50, 200, 150);
+  image(dvdImage, imageX, 50, 200, 150);
+  
+  imageX += 1;
 }
