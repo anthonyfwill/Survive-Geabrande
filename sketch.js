6ae151e6d286bb0286a5b436b4c7e11ea3642d34
Glitch (hello-webpage) <none>
2020-07-07 05:05:16 +0000

    🐙⏰ Checkpoint ./sketch.js:15945968/4152

--- sketch.js
+++ sketch.js
@@ -1,33 +1,44 @@
-/* global createCanvas background image loadImage */
+/* global createCanvas background image loadImage windowWidth 
+   windowHeight width height */
 
 let dvdImage, imageX, xVelocity, imageY, yVelocity;
+const masterVelocity = 3,
+  imageWidth = 200,
+  imageHeight = 150;
 
-function setup(){
-  createCanvas(400, 400);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
   imageX = 0;
-  xVelocity = 3;
+  imageY = 0;
+
+  xVelocity = masterVelocity;
+  yVelocity = masterVelocity;
+
   // We only want to load the logo once.
-  dvdImage = loadImage("https://cdn.glitch.com/2c8c05ca-f0b5-426e-9d1a-07a2d87bd9f6%2Fdvd-video.jpeg?v=1594094901767");
+  dvdImage = loadImage(
+    "https://cdn.glitch.com/2c8c05ca-f0b5-426e-9d1a-07a2d87bd9f6%2Fdvd-video.jpeg?v=1594094901767"
+  );
 }
 
-function draw(){
+function draw() {
   background(220);
   // Draw the logo at the new position.
-  image(dvdImage, imageX, 50, 200, 150);
-  
+  image(dvdImage, imageX, imageY, imageWidth, imageHeight);
+
   imageX += xVelocity;
-  
-  if (imageX < 200) {
-    xVelocity = -3;
+
+  if (imageX > width - imageWidth) {
+    xVelocity = -masterVelocity;
   } else if (imageX < 0) {
-    xVelocity = 3;
+    xVelocity = masterVelocity;
   }
-  
+
   imageY += yVelocity;
-  
-  if (imageY < 200) {
-    yVelocity = -3;
+
+  if (imageY > height - imageHeight) {
+    yVelocity = -masterVelocity;
   } else if (imageY < 0) {
-    yVelocity = 3;
+    yVelocity = masterVelocity;
   }
 }
