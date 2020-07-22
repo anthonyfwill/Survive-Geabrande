6ed1e9cf3ac68b6044eb286ba6a5f5a11e2a0ae0
Glitch (hello-webpage) <none>
2020-07-22 16:27:39 +0000

    💡😻 Checkpoint ./sketch.js:15945968/2911

--- sketch.js
+++ sketch.js
@@ -1,44 +1,48 @@
-/* global createCanvas background image loadImage windowWidth 
-   windowHeight width height */
+/* global p5 */
 
-let dvdImage, imageX, xVelocity, imageY, yVelocity;
-const masterVelocity = 3,
-  imageWidth = 200,
-  imageHeight = 150;
+function dvdSketch(p5instance) {
+  let dvdImage, imageX, xVelocity, imageY, yVelocity;
+  const masterVelocity = 3,
+    imageWidth = 200,
+    imageHeight = 150;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+  p5instance.setup = () => {
+    p5instance.createCanvas(300, 300);
 
-  imageX = 0;
-  imageY = 0;
+    imageX = 0;
+    imageY = 0;
 
-  xVelocity = masterVelocity;
-  yVelocity = masterVelocity;
-
-  // We only want to load the logo once.
-  dvdImage = loadImage(
-    "https://cdn.glitch.com/2c8c05ca-f0b5-426e-9d1a-07a2d87bd9f6%2Fdvd-video.jpeg?v=1594094901767"
-  );
-}
-
-function draw() {
-  background(220);
-  // Draw the logo at the new position.
-  image(dvdImage, imageX, imageY, imageWidth, imageHeight);
-
-  imageX += xVelocity;
-
-  if (imageX > width - imageWidth) {
-    xVelocity = -masterVelocity;
-  } else if (imageX < 0) {
     xVelocity = masterVelocity;
+    yVelocity = masterVelocity;
+
+    // We only want to load the logo once.
+    dvdImage = p5instance.loadImage(
+      "https://cdn.glitch.com/2c8c05ca-f0b5-426e-9d1a-07a2d87bd9f6%2Fdvd-video.jpeg?v=1594094901767"
+    );
   }
 
-  imageY += yVelocity;
+  p5instance.draw = () => {
+    p5instance.background(220);
+    // Draw the logo at the new position.
+    p5instance.image(dvdImage, imageX, imageY, imageWidth, imageHeight);
 
-  if (imageY > height - imageHeight) {
-    yVelocity = -masterVelocity;
-  } else if (imageY < 0) {
-    yVelocity = masterVelocity;
+    imageX += xVelocity;
+
+    if (imageX > p5instance.width - imageWidth) {
+      xVelocity = -masterVelocity;
+    } else if (imageX < 0) {
+      xVelocity = masterVelocity;
+    }
+
+    imageY += yVelocity;
+
+    if (imageY > p5instance.height - imageHeight) {
+      yVelocity = -masterVelocity;
+    } else if (imageY < 0) {
+      yVelocity = masterVelocity;
+    }
   }
 }
+
+let mySketch = new p5(dvdSketch);
+let mySketch2 = new p5(dvdSketch);
