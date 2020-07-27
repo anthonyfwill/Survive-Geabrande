bf79f8a543aeeb8ec8482470f072870fc791c6e4
Glitch (hello-webpage) <none>
2020-07-27 17:36:42 +0000

    🐦📅 Checkpoint ./sketch.js:18051740/2321:18574398/2943 ./.glitch-assets:18051740/1216

--- sketch.js
+++ sketch.js
@@ -3,35 +3,32 @@
           collideCircleCircle, text, textSize, mouseX, mouseY, strokeWeight, line, 
           mouseIsPressed, translate, rotate, createVector, windowWidth, windowHeight, noStroke, sqrt */
 
-let spaceship, alien, player, bulletalien1, bulletalien2, bulletspaceship, ball1;
+let spaceship, alien, player, bulletalien1, bulletalien2, bulletspaceship;
 
-function preload() {
- 
-}
+function preload() {}
 
 function setup() {
   createCanvas(800, 800);
-  // We only want to load the logo once.
-   spaceship = loadImage(
-    "https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2Fspaceship.gif?v=1595868040731"
+   
+  spaceship = loadImage(
+    "https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2Fspaceship.gif?v=1595871307227"
   );
   alien = loadImage(
     "https://cdnb.artstation.com/p/assets/images/images/006/503/665/original/william-robinson-gun-alien-passive-gif.gif?1499108527"
   );
-  image(alien, 400, 400, 50, 50);
+  player = new Ship();
 }
-
 
 function draw() {
-  image(alien, 400, 400, 50, 50);
-//player.show();
+  background(0);
+  player.show();
 }
-
 
 function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
     player.move(1);
-  } else if (keyCode === LEFT_ARROW) {
+  } 
+  if (keyCode === LEFT_ARROW) {
     player.move(-1);
   }
   if (keyCode === UP_ARROW) {
@@ -44,12 +41,24 @@ function Ship() {
   this.y = 200;
   this.scl = 100;
   this.move = function(dir) {
-    this.x += 5*dir;
+    this.x += 10 * dir;
   };
   this.show = function() {
-    
-    
     image(alien, this.x, this.y, this.scl, this.scl);
   };
 }
 
+function Laser(){
+  
+}
+
+
+function Alien() {
+  this.x = 200;
+  this.y = 200;
+  this.scl = 100;
+  
+  this.showIt = function() {
+    image(alien, this.x, this.y, this.scl, this.scl);
+  };
+}
