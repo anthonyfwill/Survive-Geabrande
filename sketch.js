eacdb5e5f066874f0edd74197c59612a035cbab7
Glitch (hello-webpage) <none>
2020-07-27 16:50:32 +0000

    🌎🍚 Checkpoint ./sketch.js:18051740/1696:18574398/3105 ./.glitch-assets:18051740/381

--- sketch.js
+++ sketch.js
@@ -1,26 +1,36 @@
 /*global createCanvas, colorMode, HSB, width, height, random, background, fill, 
           color, random, rect, ellipse, stroke, image, loadImage, keyCode,
           collideCircleCircle, text, textSize, mouseX, mouseY, strokeWeight, line, 
-          mouseIsPressed, translate, createVector, windowWidth, windowHeight, noStroke, UP_ARROW sqrt */
-let spaceship, alien;
+          mouseIsPressed, translate, rotate, createVector, windowWidth, windowHeight, noStroke, UP_ARROW sqrt */
+let spaceship, alien, player, bulletalien;
+
+function preload(){
+  spaceship = loadImage("https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2Fspaceship.gif?v=1595868040731");
+  alien = loadImage("https://cdnb.artstation.com/p/assets/images/images/006/503/665/original/william-robinson-gun-alien-passive-gif.gif?1499108527");
+}
+
 function setup() {
   colorMode(HSB, 360, 360);
-  createCanvas(400,400);
+  createCanvas(400, 400);
   background(0);
-  spaceship = loadImage('');
+  
 }
 
 function draw() {
-  
+  player = new ship();
+  player.create();
 }
 
-function ship (){
-  this.pos = createVector(width/2, height/2);
-  this.scl = 20;
+function ship() {
+  this.x = 200;
+  this.y = 200;
+  this.scl = 50;
   
-  this.create = function(){
-    translate(this.pos.x, this.pos.y);
-    rect()
-    //image(spaceship, 0, 0, this.scl, this.scl);
-  }
+  this.create = function() {
+    
+    
+    image(alien, this.x, this.y, this.scl, this.scl);
+  };
 }
+
+
