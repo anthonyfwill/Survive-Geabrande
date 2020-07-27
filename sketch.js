0b15975cb290415fd696470c8439a3639a154937
Glitch (hello-webpage) <none>
2020-07-27 17:25:23 +0000

    💂⛵️ Checkpoint ./sketch.js:18051740/10641:18574398/110

--- sketch.js
+++ sketch.js
@@ -2,34 +2,32 @@
           color, random,  LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, UP_ARROW, rect, ellipse, stroke, image, loadImage, keyCode,
           collideCircleCircle, text, textSize, mouseX, mouseY, strokeWeight, line, 
           mouseIsPressed, translate, rotate, createVector, windowWidth, windowHeight, noStroke, sqrt */
-let spaceship, alien, player, bulletalien1, bulletalien2, bulletspaceship;
+
+let spaceship, alien, player, bulletalien1, bulletalien2, bulletspaceship, ball1;
 
 function preload() {
-  spaceship = loadImage(
-    "https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2Fspaceship.gif?v=1595868040731"
-  );
-  alien = loadImage(
-    "https://cdnb.artstation.com/p/assets/images/images/006/503/665/original/william-robinson-gun-alien-passive-gif.gif?1499108527"
-  );
+ 
 }
 
 function setup() {
-  colorMode(HSB, 360, 360);
-  createCanvas(400, 400);
-  background(95);
-}
-
-function draw() {
-  spaceship = loadImage(
+  createCanvas(800, 800);
+  // We only want to load the logo once.
+   spaceship = loadImage(
     "https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2Fspaceship.gif?v=1595868040731"
   );
   alien = loadImage(
     "https://cdnb.artstation.com/p/assets/images/images/006/503/665/original/william-robinson-gun-alien-passive-gif.gif?1499108527"
   );
-  /*player = new ship();
-  player.show();
-  keyPressed();*/
+  image(alien, 400, 400, 50, 50);
 }
+
+
+function draw() {
+  image(alien, 400, 400, 50, 50);
+//player.show();
+}
+
+
 function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
     player.move(1);
@@ -41,7 +39,7 @@ function keyPressed() {
   }
 }
 
-function ship() {
+function Ship() {
   this.x = 200;
   this.y = 200;
   this.scl = 100;
@@ -49,8 +47,9 @@ function ship() {
     this.x += 5*dir;
   };
   this.show = function() {
-    fill(255);
+    
     
     image(alien, this.x, this.y, this.scl, this.scl);
   };
 }
+
