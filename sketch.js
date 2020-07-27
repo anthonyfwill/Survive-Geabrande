483b593014db0603514f8361626504d398ff3130
Glitch (hello-webpage) <none>
2020-07-27 17:59:04 +0000

    🌘🐛 Checkpoint ./sketch.js:18051740/3129:18574398/645 ./.glitch-assets:18051740/1006

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 /*global createCanvas, colorMode, HSB, width, height, random, background, fill, 
           color, random,  LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, UP_ARROW, rect, ellipse, stroke, image, loadImage, keyCode,
           collideCircleCircle, text, textSize, mouseX, mouseY, strokeWeight, line, 
-          mouseIsPressed, translate, rotate, createVector, windowWidth, windowHeight, noStroke, sqrt */
+          mouseIsPressed, translate, point, rotate, createVector, windowWidth, windowHeight, noStroke, sqrt */
 
 let spaceship,
   alien,
@@ -9,16 +9,18 @@ let spaceship,
   enemy,
   bulletalien1,
   bulletalien2,
-  bulletspaceship;
+  bulletspaceship,
+    lasers;
 
 function preload() {}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  lasers = [];
   spaceship = loadImage(
-    "https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2Fspaceship2.gif?v=1595871992652"
+    "https://cdn.glitch.com/f110bdf6-83ea-4102-a2d6-396da3461187%2F8-bit-spaceship-png-1.png?v=1595872548652"
   );
+  
   alien = loadImage(
     "https://cdnb.artstation.com/p/assets/images/images/006/503/665/original/william-robinson-gun-alien-passive-gif.gif?1499108527"
   );
@@ -30,6 +32,11 @@ function draw() {
   background(0);
   enemy.Ashow();
   player.show();
+  for(var i = 0; i< lasers.length; i++){
+    lasers[i].show();
+    lasers[i].update();
+    
+  }
 }
 
 function keyPressed() {
@@ -39,30 +46,43 @@ function keyPressed() {
   if (keyCode === LEFT_ARROW) {
     player.v = -3;
   }
+  if(keyCode === 32){
+    lasers.push(new Laser(player.x));
+  }
 }
 
 function Ship() {
   this.x = windowWidth/2;
-  this.y = windowHeight - scl;
-  this.scl = 100;
+  this.scl = 50;
+  this.y = windowHeight - this.scl;
+  
   this.v = 0;
 
   this.show = function() {
     fill(255);
-    //image(spaceship, this.x, this.y, this.scl, this.scl);
-    rect(this.x, this.y, this.scl ,this.scl);
+    image(spaceship, this.x, this.y, this.scl, this.scl);
+    // rect(this.x, this.y, this.scl ,this.scl);
     this.x += this.v;
   };
 }
 
 function Laser(xpos) {
-  this.x = xpos;
+  this.x = xpos+3;
   this.y = windowHeight - player.scl;
+  this.v = -5;
+  this.update = function(){
+    this.y += this.v;
+  };
+  this.show = function(){
+    stroke(255);
+    strokeWeight(2);
+    point(this.x, this.y);
+  }
 }
 
 function Alien() {
   this.Ax = 200;
-  this.Ay = 200;
+  this.Ay = 0;
   this.Ascl = 100;
 
   this.Ashow = function() {
